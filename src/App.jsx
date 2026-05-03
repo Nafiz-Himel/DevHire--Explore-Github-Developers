2b2dc55bfd29c973468f09f9c39e1abb19857d74
Nafiz Himel <nafizhimel7@gmail.com>
2026-05-03 17:49:38 +0600

    logout-issue-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,40 @@ import Developers from './components/Developers'
 import Shortlist from './components/Shortlist'
 
 function App() {
-  const [signedIn, setSignedIn] = useState(false)
+  const [signedIn, setSignedIn] = useState(() =>
+    localStorage.getItem("isSignedIn") === "true"
+  )
   const [currentPage, setCurrentPage] = useState('dashboard')
 
   const handleNavigate = (page) => {
     setCurrentPage(page)
   }
 
+  const handleSignOut = () => {
+    localStorage.removeItem("isSignedIn")
+    setSignedIn(false)
+  }
+
   if (!signedIn) {
-    return <Login onSignIn={() => setSignedIn(true)} />
+    return <Login onSignIn={() => {
+      localStorage.setItem("isSignedIn", "true")
+      setSignedIn(true)
+    }} />
   }
 
   if (currentPage === 'dashboard') {
-    return <Dashboard onSignOut={() => setSignedIn(false)} onNavigate={handleNavigate} />
+    return <Dashboard onSignOut={handleSignOut} onNavigate={handleNavigate} />
   }
 
   if (currentPage === 'developers') {
-    return <Developers onSignOut={() => setSignedIn(false)} onNavigate={handleNavigate} />
+    return <Developers onSignOut={handleSignOut} onNavigate={handleNavigate} />
   }
 
   if (currentPage === 'shortlist') {
-    return <Shortlist onSignOut={() => setSignedIn(false)} onNavigate={handleNavigate} />
+    return <Shortlist onSignOut={handleSignOut} onNavigate={handleNavigate} />
   }
 
-  return <Dashboard onSignOut={() => setSignedIn(false)} onNavigate={handleNavigate} />
+  return <Dashboard onSignOut={handleSignOut} onNavigate={handleNavigate} />
 }
 
 export default App
